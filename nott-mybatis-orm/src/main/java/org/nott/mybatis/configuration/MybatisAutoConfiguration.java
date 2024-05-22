5008b1f1af721b3c8be5b248ce486be57828b7e8
Nott <nott@163.com>
2024-05-22 10:58:13 +0800

    todo: Verify that external applications can automatically assemble CommonMapper's bean when they introduce mybatis-orm.

--- nott-mybatis-orm/src/main/java/org/nott/mybatis/configuration/MybatisAutoConfiguration.java
+++ nott-mybatis-orm/src/main/java/org/nott/mybatis/configuration/MybatisAutoConfiguration.java
@@ -3,13 +3,16 @@ package org.nott.mybatis.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.nott.mybatis.NottMapperScannerRegistrar;
 import org.nott.mybatis.config.NottMybatisConfig;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.AnnotationMetadata;
 
 
 import javax.sql.DataSource;
@@ -36,6 +39,14 @@ public class MybatisAutoConfiguration {
         sqlSessionFactoryBean.setMapperLocations(resolver.getResources(nottMybatisConfig.getMapperLocation()));
         sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(nottMybatisConfig.getMybatisConfigLocation()));
         return sqlSessionFactoryBean;
+    }
+
+    // todo 验证外部应用加载bean上下文时是否自动注册CommonMapper bean
+    @Bean("NottMapperScannerRegistrar")
+    public NottMapperScannerRegistrar registrar(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry){
+        NottMapperScannerRegistrar registrar = new NottMapperScannerRegistrar();
+        registrar.registerBeanDefinitions(importingClassMetadata, registry);
+        return registrar;
     }
 
 }
